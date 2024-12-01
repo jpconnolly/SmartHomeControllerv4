8aee4abba26606af6b7ad2785eb8406b5434cca2
jp.connolly <jp.connolly@ulster.ac.uk>
2024-12-01 11:04:54 +0000

    Initial commit

---
+++ SmartHomeControllerv4/SmartHomeController.cs
@@ -0,0 +1,149 @@
+﻿using SmartHomeController;
+using System.Net.NetworkInformation;
+
+public class Program
+{
+    // Globally available variables within the SmartHomeController file
+    private static List<SmartDevice> devices = new List<SmartDevice>();
+    static string destinationFilePath;
+
+    // Starting point for the application
+    public static void Main()
+    {
+        string folder = "Data";
+        string filename = "smartdevices.csv";
+
+        destinationFilePath = CopyDataToWorkingDir(folder, filename);
+        LoadSmartDevices(destinationFilePath);
+        MainMenu();
+    }
+
+    // Copy csv file from the Data folder to the path
+    // C:\Users\user profile\source\repos\SmartHomeControllerv4\SmartHomeControllerv4\bin\Debug\net8.0
+    // Note that this method overwrites the csv file stored in the "net8.0" folder
+    public static string CopyDataToWorkingDir(string folder, string filename)
+    {
+        // Define the source and destination paths
+        string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        string sourceFilePath = Path.Combine(projectDirectory, folder, filename);
+        string destinationFilePath = Path.Combine(Environment.CurrentDirectory, filename);
+
+        // Copy the file to the working directory
+        if (File.Exists(sourceFilePath))
+        {
+            File.Copy(sourceFilePath, destinationFilePath, true);
+        }
+        else
+        {
+            Console.WriteLine("Source file not found: " + sourceFilePath);
+
+        }
+        // Retrun the path to the copied file
+        return destinationFilePath;
+    }
+
+    public static void LoadSmartDevices(string destinationFilePath)
+    {
+        using (var reader = new StreamReader(destinationFilePath))
+        {
+            // Skip the header line
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                // Need to convert missing data into 0 for
+                // double and int
+                if (values[3].Length == 0)
+                {
+                    values[3] = "0";
+                }
+                if (values[6].Length == 0)
+                {
+                    values[6] = "0";
+                }
+                if (values[7].Length == 0)
+                {
+                    values[7] = "0";
+                }
+                if (values[8].Length == 0)
+                {
+                    values[8] = "0";
+                }
+                // read each value into the relevant 
+                // variable 
+                int deviceID = int.Parse(values[0]);
+                string deviceName = values[2];
+                string deviceType = values[1];
+                double brightness = Convert.ToDouble((values[3]));
+                string colour = values[4];
+                string cameraResolution = values[5];
+
+                double currentTemperature = Convert.ToDouble(values[6]);
+                double targetTemperature = Convert.ToDouble(values[7]);
+                int volume = int.Parse(values[8]);
+
+                SmartDevice device = null;
+
+                // Device type is read from the csv file
+                switch (deviceType)
+                {
+                    case "SmartLight":
+                        device = new SmartLight(deviceID, deviceName, brightness, colour);
+                        break;
+                    case "SmartSecurityCamera":
+                        device = new SmartSecurityCamera(deviceID, deviceName, cameraResolution);
+                        break;
+                    case "SmartThermostat":
+                        device = new SmartThermostat(deviceID, deviceName, currentTemperature, targetTemperature);
+                        break;
+                    case "SmartSpeaker":
+                        device = new SmartSpeaker(deviceID, deviceName, volume);
+                        break;
+                }
+                // add the device into the list of devices
+                if (device != null)
+                {
+                    devices.Add(device);
+                }
+            }
+        }
+    }
+
+    public static void MainMenu()
+    {
+        while (true)
+        {           
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("1. Install new device");
+            Console.WriteLine("2. Control a device");
+            Console.WriteLine("3. View all devices");
+            Console.WriteLine("4. Exit");
+
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    //InstallDeviceMenu();
+                    break;
+                case "2":
+                    //ControlDevicesMenu();
+                    break;
+                case "3":
+                    //ViewAllDevices();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+
+
+}
---
+++ SmartHomeControllerv4/SmartSpeaker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeController
+{
+    public class SmartSpeaker : SmartDevice
+    {
+        // Private fields
+        private int volume;
+        private bool isPlaying;
+
+        // Public properties
+        public int Volume
+        {
+            get { return volume; }
+            set { volume = value; }
+        }
+
+        public bool IsPlaying
+        {
+            get { return isPlaying; }
+            set { isPlaying = value; }
+        }
+
+        // Constructor
+        public SmartSpeaker(int speakerDeviceID, string speakerDeviceName, int speakerVolume) : base(speakerDeviceID, speakerDeviceName)
+        {
+            this.Volume = speakerVolume;
+
+        }
+
+        public override void GetStatus()
+        {
+            base.GetStatus();
+            Console.WriteLine("This is a smart speaker.");
+            Console.WriteLine($"Volume: {Volume}, Play status (Yes or No): {(IsPlaying ? "YES" : "NO")}");
+        }
+
+    }
+}
